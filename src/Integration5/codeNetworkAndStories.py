803c51e92a70271e5bfa6a338bff320ef68b5567
n-aretz <42115929+n-aretz@users.noreply.github.com>
2021-12-14 00:44:38 +0100

    Add another zone and build in finish story

--- src/Integration5/codeNetworkAndStories.py
+++ src/Integration5/codeNetworkAndStories.py
@@ -5,6 +5,7 @@ import board
 import busio
 import time
 import neopixel
+import random
 
 from adafruit_esp32spi import adafruit_esp32spi
 from DFPlayer import DFPlayer
@@ -27,52 +28,67 @@ i2c = busio.I2C(board.SCL, board.SDA)
 mpr121 = adafruit_mpr121.MPR121(i2c)
 
 # Setup dfplayer
-PLAYER_VOL = 50
+PLAYER_VOL = 80
 dfplayer = DFPlayer(volume=PLAYER_VOL)
 
 # Setup neopixels
 pixel_pin = board.A1
 num_pixels = 24
-pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=1.0, auto_write=False)
-
+pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=0.9, auto_write=False)
 
 # Setup global flags
 currZone = None
 flag = False
+timePast = 0
 
 # ---------------------------------------------End Setup--------------------------------
 
 # Fade from yellow to orange
 def fade(x, y, z):
-    global flag, currZone
+    global flag, currZone, pixels
     for i in range(x, y, z):
         pixels.fill((255, i, 0))
         pixels.show()
-        time.sleep(0.01)
+        time.sleep(0.05)
         if not touched():
             dfplayer.stop()
+            pixels.deinit()
+            pixels = neopixel.NeoPixel(
+                pixel_pin, num_pixels, brightness=0.9, auto_write=False
+            )
             flag = False
             break
         if not flag:
-            if currZone == 'ESP02':
-                # playConfirmation()
-                playStory(1)
+            if currZone == "ESP02":
+                playConfirmation()
+                playStory(1, 3)
                 flag = True
-            if currZone == 'ESP01':
-                # playConfirmation()
-                playStory(2)
+            if currZone == "ESP01":
+                playConfirmation()
+                playStory(4, 6)
                 flag = True
+            # if currZone == 'ESP03':
+        #                 playConfirmation()
+        #                 playStory(5, 7)
+        #                 flag = True
         checkNetwork()
+
 
 # Fade from blue to turqoise
 def idle(x, y, z):
+    global pixels
     for i in range(x, y, z):
         pixels.fill((0, 255, i))
         pixels.show()
-        time.sleep(0.01)
+        time.sleep(0.05)
         if touched():
+            pixels.deinit()
+            pixels = neopixel.NeoPixel(
+                pixel_pin, num_pixels, brightness=0.1, auto_write=False
+            )
             fade(140, 255, 1)
             fade(255, 140, -1)
+
 
 # Returns true when the touch sensor is being touched
 def touched():
@@ -93,20 +109,25 @@ def touched():
 # Chooses a story randomly out of the given range and plays that story
 # a = from
 # b = to
-def playStory(a):
-    dfplayer.play(track=a)
+def playStory(a, b):
+    global timePast
+    timePast = time.monotonic()
+    i = random.randint(a, b)
+    dfplayer.play(track=i)
     print("Play story")
 
+
 # Plays the confirmation sound
-# def playConfirmation():
-#     dfplayer.set_volume(percent=50)
-#     dfplayer.play(track=4)
-#     time.sleep(2)
-#     dfplayer.set_volume(percent=40)
+def playConfirmation():
+    dfplayer.set_volume(percent=100)
+    dfplayer.play(track=7)
+    time.sleep(2)
+    dfplayer.set_volume(percent=80)
+
 
 # Plays the story that corresponds to the closest network
 def checkNetwork():
-    global currZone, flag
+    global currZone, flag, timePast
     closest = None
     closest_rssi = -80
 
@@ -116,25 +137,27 @@ def checkNetwork():
             dfplayer.stop()
             flag = False
             return
-        if (
-            str(entry["ssid"], "utf-8") == 'ESP02' 
-            or str(entry["ssid"], "utf-8") == 'ESP01'
+        elif (
+            str(entry["ssid"], "utf-8") == "ESP02"
+            or str(entry["ssid"], "utf-8") == "ESP01"
+            # or str(entry["ssid"], "utf-8") == 'ESP03'
         ):
             if str(entry["ssid"], "utf-8") == closest:
                 pass
-            elif entry["rssi"] > closest_rssi:
+            elif entry["rssi"] > closest_rssi and (timePast - 31000) <= 0:
                 closest = str(entry["ssid"], "utf-8")
                 closest_rssi = entry["rssi"]
-            else:
-                continue
 
     # Play stories to the current broadcaster
-    if closest == 'ESP02' and closest is not currZone:
-        # playConfirmation()
-        playStory(1)
-    if closest == 'ESP01' and closest is not currZone:
-        # playConfirmation()
-        playStory(2)
+    if closest == "ESP02" and closest is not currZone:
+        playConfirmation()
+        playStory(1, 3)
+    if closest == "ESP01" and closest is not currZone:
+        playConfirmation()
+        playStory(4, 6)
+    # if closest == 'ESP03' and closest is not currZone:
+    #         playConfirmation()
+    #         playStory(3)
     currZone = closest
 
 
